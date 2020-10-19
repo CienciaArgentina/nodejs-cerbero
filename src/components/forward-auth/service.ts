ba7486a30e188b825c32a7b2146b951c0b3e78d6
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-10-19 20:20:31 -0300

    minor change

--- src/components/forward-auth/service.ts
+++ src/components/forward-auth/service.ts
@@ -1,5 +1,5 @@
 import { verify } from 'jsonwebtoken';
-import { HTTP400Error, HttpValidationError } from 'ciencia-argentina-backend-commons';
+import { HTTP400Error, HTTP401Error, HttpValidationError } from 'ciencia-argentina-backend-commons';
 import { Roles, ForwardAuth, JwtToken } from './models';
 import dotenv from 'dotenv';
 import { validateForwardAuthScheme } from './utils/validators/post';
@@ -17,12 +17,13 @@ export const verifyToken = (forwardAuthRequest: ForwardAuth): JwtToken => {
   if (errors) throw new HttpValidationError(errors);
   const decoded: JwtToken | string = verify(jwt, process.env.JWT_SIGNATURE || '');
 
+  //https://tools.ietf.org/html/rfc6750
   if (!decoded || typeof decoded === 'string' || !decoded.expires_at || !decoded.roles)
-    throw new HTTP400Error('invalid_token');
+    throw new HTTP401Error('invalid_token');
 
-  if (decoded.expires_at < new Date().getTime() / 1000) throw new HTTP400Error('token expired');
+  if (decoded.expires_at < new Date().getTime() / 1000) throw new HTTP401Error('token expired');
 
-  if (!includeClaims(decoded.roles, required_claim)) throw new HTTP400Error('not included claims');
+  if (!includeClaims(decoded.roles, required_claim)) throw new HTTP401Error('not included claims');
 
   return decoded;
 };
